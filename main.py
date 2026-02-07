807859af47c615de9cdae3d7383757fb42bf6a89
sebastia660 <sebastian.cumpiano@upr.edu>
2026-02-07 15:14:36 -0400

    Task 4 completed

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ while running:
             # TODO 4: Fixing the scoring
             # When you pass through the pipes the score should be updated to the current score + 1. Implement the
             # logic to accomplish this scoring system.
-            score = 1
+            score += 1
 
         if bird_y > 600 or bird_y < 0:
             game_over = True
