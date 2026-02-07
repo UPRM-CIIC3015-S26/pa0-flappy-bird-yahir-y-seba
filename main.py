5917bbc7dc880e874bbdde6260504c78b231881d
sebastia660 <sebastian.cumpiano@upr.edu>
2026-02-07 08:17:50 -0400

    Task 1 completed

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ bird_y = 300
 bird_velocity = 0
 # TODO 1: Tweaking the physics
 # Looks like the player is falling too quickly not giving a change to flap it's wing, maybe tweak around with the value of this variable
-gravity = 9.81
+gravity = 0.5
 jump = -10
 # Pipe Variables -->
 pipe_x = 400
